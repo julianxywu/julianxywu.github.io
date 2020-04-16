aed345c4eb5809063cb9e5ba8409fb1e52c9eea0
Julian Wu <julian.22@dartmouth.edu>
2020-04-16 19:14:11 -0400

    pushing final version of SA3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let $num = 0;
+
+function incrementTime() {
+  $num += 1;
+  $('#main').html(`You've been on this page for ${$num} seconds..`);
+}
+
+setInterval(incrementTime, 1000);
