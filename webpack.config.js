8ba0ff2589c36ad5d67daa158887b834f0c8c14b
Julian Wu <julian.22@dartmouth.edu>
2020-04-16 19:31:52 -0400

    extra credit

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,22 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        // Specify enforce: 'pre' to apply the loader
+        // before url-loader/svg-url-loader
+        // and not duplicate it in rules with them
+        enforce: 'pre',
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          // Images larger than 10 KB won’t be inlined
+          limit: 10 * 1024,
+        },
+      },
     ],
   },
   plugins: [
