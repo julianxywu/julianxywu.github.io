faa4ac8895e651e181add3086c993a5c1903a236
Julian Wu <julian.22@dartmouth.edu>
2020-04-28 04:14:15 -0400

    added routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
 // change require to es6 import style
-import $ from 'jquery';
+// import $ from 'jquery';
 import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
 
-let $num = 0;
 
-function incrementTime() {
-  $num += 1;
-  $('#main').html(`You've been on this page for ${$num} seconds..`);
-}
+// const $num = 0;
 
-setInterval(incrementTime, 1000);
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
+
+
+// function incrementTime() {
+//   $num += 1;
+//   $('#main').html(`You've been on this page for ${$num} seconds..`);
+// }
+
+// setInterval(incrementTime, 1000);
