14bb369afe6f07693a02f8d4208e28c5eed56988
Julian Wu <julian.22@dartmouth.edu>
2020-05-18 01:57:37 -0400

    react build

---
+++ build/precache-manifest.862ae59fa7c58595e526bbbc416f1c41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a4c786a7f0fb945212d05ba80d79da",
+    "url": "/julianxywu.me/index.html"
+  },
+  {
+    "revision": "550f0891452946fa690a",
+    "url": "/julianxywu.me/static/css/main.86898007.chunk.css"
+  },
+  {
+    "revision": "0cf2c7023279ff5c21e4",
+    "url": "/julianxywu.me/static/js/2.d9609397.chunk.js"
+  },
+  {
+    "revision": "892c5a5128f54a613716d73f6973b236",
+    "url": "/julianxywu.me/static/js/2.d9609397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550f0891452946fa690a",
+    "url": "/julianxywu.me/static/js/main.be2d07d4.chunk.js"
+  },
+  {
+    "revision": "ea2e1356fc6afcb0ee8a",
+    "url": "/julianxywu.me/static/js/runtime-main.08ea2360.js"
+  }
+]);
